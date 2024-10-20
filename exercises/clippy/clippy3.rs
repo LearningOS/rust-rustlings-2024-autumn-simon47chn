74848fda137a35fb3222eae561d8454ef7af674b
simon47chn <simon.jiasiqi@qq.com>
2024-10-20 14:16:20 +0800

    change clippy3 error

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -9,20 +9,21 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        panic!("my_option should not be a none");
-    }
+    // if let None = my_option {
+    //     panic!("my_option should not be a none");
+    // }
 
     let my_arr = &[-1, -2, -3,-4, -5, -6];
-    println!("My array! Here it is: {:?}", my_arr);
-    let empty_vec: Vec<i32> = Vec::new();
-    println!("This Vec is empty, see? {:?}", empty_vec);
+    println!("My array! Here it is: {my_arr:?}",);
+    let mut empty_vec: Vec<i32> = vec![1, 2, 3];
+    empty_vec.clear();
+    println!("This Vec is empty, see? {empty_vec:?}");
 
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
     // value_a = value_b;
     // value_b = value_a;
-    std::mem::swap(&mut value_a, &mut value_b);
+    std::mem::swap(&mut value_b, &mut value_a);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
